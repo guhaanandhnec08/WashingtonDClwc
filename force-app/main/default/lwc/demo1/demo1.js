c9ba0e24ee84cd22bff783c971958051b44b5122
garumugam <garumugam@acumensolutions.com>
2019-02-07 18:42:53 -0500

    Changes as of Feb-7

--- force-app/main/default/lwc/demo1/demo1.js
+++ force-app/main/default/lwc/demo1/demo1.js
@@ -2,9 +2,36 @@ import {
     LightningElement,
     track
 } from 'lwc';
+
+import trailheadLogo from '@salesforce/resourceUrl/trailhead_logo';
+import trailheadCharacters from '@salesforce/resourceUrl/trailhead_characters';
+
 export default class HelloWorld extends LightningElement {
-    @track greeting = 'World';
+
+    //Decorator Declarations
+    @track renderEventStr = false;
+    @track firstName = '';
+    @track lastName = '';
+    @track eventName = '';
+
+    // Expose the static resource URL for use in the template
+    trailheadLogoUrl = trailheadLogo;
+    // Expose URL of assets included inside an archive file
+    einsteinUrl = trailheadCharacters + '/images/einstein.png';
+
+    //Function 
     changeHandler(event) {
-        this.greeting = event.target.value;
+        const field = event.target.name;
+
+        if (field === 'firstName') {
+            this.firstName = (event.target.value).trim().toUpperCase();
+        } else if (field === 'lastName') {
+            this.lastName = (event.target.value).trim().toUpperCase();
+        } else if (field === 'eventName') {
+            this.eventName = event.target.value;
+            if (event.target.value !== '' && event.target.value != null && event.target.value != undefined) {
+                this.renderEventStr = true;
+            }
+        }
     }
 }
